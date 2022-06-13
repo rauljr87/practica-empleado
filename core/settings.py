89f7dc992af2251e331dc7481396617449709a04
r4u1 <raul.iturraldec@gmail.com>
2022-06-12 23:00:40 -0500

    se crea app departamento y persona

--- core/settings.py
+++ core/settings.py
@@ -33,8 +33,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'core'
+    # local apps
+    'core',
+    'departamento',
+    'persona'
 ]
 
 MIDDLEWARE = [
