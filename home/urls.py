147692fdb0993534bf539b6c8c7cb1fec74b792c
r4u1 <raul.iturraldec@gmail.com>
2022-06-14 22:15:00 -0500

    se crea View PruebaListView con su template

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PruebaView
+from .views import PruebaView, PruebaListView
 
 
 app_name = 'home'
 
 urlpatterns = [
-    path('prueba/', PruebaView.as_view(), name='Prueba')
+    path('prueba/', PruebaView.as_view(), name='prueba'),
+    path('lista/', PruebaListView.as_view(), name='lista')
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 # vistas genéricas
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 
 class PruebaView(TemplateView):
     template_name = 'home/prueba.html'
+
+
+class PruebaListView(ListView):
+    template_name = 'home/lista.html'
+    context_object_name = 'listaNumeros'
+    queryset = ['0', '10', '20', '30']
