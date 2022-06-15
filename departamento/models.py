26563651b954aad32f8ac045f099144a1351b960
r4u1 <raul.iturraldec@gmail.com>
2022-06-15 06:13:45 -0500

    se migra
    se crea model departamento

--- departamento/models.py
+++ departamento/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Departamento(models.Model):
+    """ Crea una tabla Departamento """
+
+    name = models.CharField('Nombre', max_length=100)
+    short_name = models.CharField('Nombre Corto', max_length=20)
+    anulate = models.BooleanField('Anulado', default=False)
