78d55d811000f341fd51db694c25861b18dc513d
r4u1 <raul.iturraldec@gmail.com>
2022-06-15 07:03:53 -0500

    model Empleado

--- departamento/models.py
+++ departamento/models.py
@@ -8,6 +8,6 @@ class Departamento(models.Model):
     """ Crea una tabla Departamento """
 
     # campos
-    name = models.CharField('Nombre', max_length=100)
-    short_name = models.CharField('Nombre Corto', max_length=20, blank=True)
+    name = models.CharField('Nombre', max_length=100, editable=True)
+    short_name = models.CharField('Nombre Corto', max_length=20, blank=True, unique=True)
     anulate = models.BooleanField('Anulado', default=False)
--- persona/models.py
+++ persona/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from departamento.models import Departamento
+
+# tabla para base de datos
+
+
+class Empleado(models.Model):
+    """ Tabla de datos Empleado """
+
+    JOB_CHOICES = (
+        {'0': 'CONTADOR'},
+        {'1': 'ADMINISTRADOR'},
+        {'2': 'ECONOMISTA'},
+        {'4': 'OTRO'}
+    )
+    first_name = models.CharField('Nombre', max_length=60)
+    last_name = models.CharField('Apellido', max_length=60)
+    job = models.CharField('Trabajo', max_length=50, choices=JOB_CHOICES)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
