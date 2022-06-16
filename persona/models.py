23532953167f95bcfbf36cf38ec7c30214b14596
r4u1 <raul.iturraldec@gmail.com>
2022-06-15 21:41:09 -0500

    se define función str en model Empleado

--- persona/models.py
+++ persona/models.py
@@ -9,12 +9,16 @@ class Empleado(models.Model):
     """ Tabla de datos Empleado """
 
     JOB_CHOICES = (
-        {'0': 'CONTADOR'},
-        {'1': 'ADMINISTRADOR'},
-        {'2': 'ECONOMISTA'},
-        {'4': 'OTRO'}
+        ('0', 'CONTADOR'),
+        ('1', 'ADMINISTRADOR'),
+        ('2', 'ECONOMISTA'),
+        ('4', 'OTRO')
     )
     first_name = models.CharField('Nombre', max_length=60)
     last_name = models.CharField('Apellido', max_length=60)
     job = models.CharField('Trabajo', max_length=50, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    # image = models.ImageField(upload_to=None, height_field=None, width_field=None)
+
+    def __str__(self):
+        return str(self.id) + '-' + self.first_name + '-' + self.last_name
