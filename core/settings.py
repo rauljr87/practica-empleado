a0b7033c7a03558568a26154d3ec25905c5e7921
r4u1 <raul.iturraldec@gmail.com>
2022-06-18 01:01:43 -0500

    se añade third party app django-ckeditor al apps settings de core

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party
+    'ckeditor',
+
     # local apps
     'core',
     'departamento',
