46f5f80d440ade7aa8250955e7e23ed5b7b2a8e1
r4u1 <raul.iturraldec@gmail.com>
2022-06-18 04:28:46 -0500

    persona/views
    - class ListByJobs
    - template list_by_job
    - url name list_by_job

--- persona/urls.py
+++ persona/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     ListAllEmpleados,
     ListByAreaEmpleado,
+    ListByJobs,
 )
 
 
@@ -10,5 +11,8 @@ app_name = 'persona'
 
 urlpatterns = [
     path('list_all/', ListAllEmpleados.as_view(), name='list_all'),
-    path('lis_by_area/', ListByAreaEmpleado.as_view(), name='list_by_area'),
+    # short_name, variable declarada para aplicar filtro de empleados por departamento
+    path('list_by_area/<short_name>/', ListByAreaEmpleado.as_view(), name='list_by_area'),
+    # job, variable declarada para aplicar filtro de empleados por trabajo
+    path('list_by_job/<job>/', ListByJobs.as_view(), name='list_by_job'),
 ]
--- persona/views.py
+++ persona/views.py
@@ -22,10 +22,46 @@ class ListByAreaEmpleado(ListView):
     """ Lista todos los empleados de la empresa por su área """
 
     template_name = 'persona/list_by_area.html'
-    queryset = Empleado.objects.filter(
-        departamento__short_name='informatica',
-    )
+    # filtro a través de un queryset
+    # queryset = Empleado.objects.filter(
+    #    # filtro queryset por short_name a departamento
+    #    departamento__short_name='informatica',
+    # )
+
+    # optimizando el filtro
+    def get_queryset(self):
+        """ Retorna lista filtrando los empleados por departamento """
+
+        # pasamos, mediante kwargs, la variable declarada en la url para aplicar filtro por departamento
+        area = self.kwargs['short_name']
+
+        lista = Empleado.objects.filter(
+            # filtro queryset por short_name a departamento del modelo Empleado
+            departamento__short_name=area
+        )
+        return lista
+
 
 # 3.- Listar empleados por trabajo.
+
+
+class ListByJobs(ListView):
+    """ Lista todos los empleados de la empresa por trabajo """
+
+    template_name = 'persona/list_by_job.html'
+
+    def get_queryset(self):
+        """ Retorna lista filtrando los empleados por trabajo """
+
+        # pasamos, mediante kwargs, la variable declarada en la url para aplicar filtro por trabajo
+        job = self.kwargs['job']
+
+        lista = Empleado.objects.filter(
+            # filtro queryset por job a departamento del modelo Empleado
+            job=job
+        )
+        return lista
+
+
 # 4.- Listar empleados por palabra clave.
 # 5.- Listar habilidades de un empleado.
