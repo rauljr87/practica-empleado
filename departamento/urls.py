00d63196d8f5d8b5b36e46fc6a8010201951fad4
r4u1 <raul.iturraldec@gmail.com>
2022-06-19 04:32:16 -0500

    departameto/views
    - class NewDepartamentoView
    - form_class NewDepartamentoForm
    - formulario que no depende de un modelo

--- departamento/urls.py
+++ departamento/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from .views import NewDepartamentoView
 
+# def prueba(self):
+#    return print("Prueba")
 
-def prueba(self):
-    return print("Prueba")
-
-
-app_name = 'departamento'
+app_name = 'departamento_app'
 
 urlpatterns = [
-    path('', prueba, name='prueba'),
+    # path('', prueba, name='prueba'),
+    # FormView
+    path('new_departamento/', NewDepartamentoView.as_view(), name='new_departamento'),
 ]
--- departamento/views.py
+++ departamento/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from persona.models import Empleado
+from departamento.models import Departamento
+# vista genérica para trabajar con formulario que no vinculados con un modelo directamente
+from django.views.generic.edit import FormView
+from .forms import NewDepartamentoForm
 
-# Create your views here.
+
+# FORMVIEW
+
+
+class NewDepartamentoView(FormView):
+    """
+    Crea un departamento con un empleado por medio de formulario
+    que no depende de un modelo
+    """
+
+    template_name = "departamento/new_departamento.html"
+    form_class = NewDepartamentoForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        print('******** estamos en el forma valid')
+
+        # instancia departamento
+        departamento = Departamento(
+            name=form.cleaned_data['departamento'],
+            short_name=form.cleaned_data['short_name'],
+        )
+        departamento.save()
+
+        nombre = form.cleaned_data['nombre']
+        apellido = form.cleaned_data['apellido']
+
+        Empleado.objects.create(
+            first_name=nombre,
+            last_name=apellido,
+            job='1',
+            departamento=departamento
+        )
+
+        return super(NewDepartamentoView, self).form_valid(form)
